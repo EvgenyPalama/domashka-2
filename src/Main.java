dc9bec8f98b09ef409f907cc99f88dbf2c0ac7b7
Neto test <palama15@mail.ru>
2023-04-30 09:22:28 +0500

    bonus

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 6666;
+        int bonus = amount / 20;
+        System.out.println("Количество милей: " + bonus);
+
+
+    }
+}
